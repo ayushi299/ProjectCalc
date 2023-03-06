a8b894fb077f1db8df841dcda708259646a9bded
ayushi.mittal <ayushimittal299@gmail.com>
2023-03-06 15:45:57 +0530

    Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!kk");
+        System.out.println("Hello world!llkk");
     }
 }
